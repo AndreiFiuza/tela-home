979a0a18ca004ae0678d112ad839da9b9b2f7df9
ANDREI DO ESPÍRITO SANTO FIUZA <24122362@CFP127ALUNO.LOCAL>
2025-04-03 14:09:42 -0300

    produzindo html, css e js da tela home

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     const likeButton = document.querySelector(".like-button");
     const heartIcon = likeButton.querySelector(".heart-icon");
 
     likeButton.addEventListener("click", function () {
         heartIcon.classList.toggle("liked");
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const likeButton = document.querySelector(".save-button");
+    const heartIcon = likeButton.querySelector(".save-icon");
+
+    likeButton.addEventListener("click", function () {
+        heartIcon.classList.toggle("save");
     });
 });
